2ef2d5a33ccea7845f35d3b3fa1dabd881ec07d1
notistris <notistris@gmail.com>
2026-01-03 23:09:38 +0700

    symlink

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -304,6 +304,35 @@ uint64 sys_open(void) {
             return -1;
         }
         ilock(ip);
+
+        if (ip->type == T_SYMLINK && !(omode & O_NOFOLLOW)) {
+            int depth = 0;
+            while (ip->type == T_SYMLINK) {
+                if (depth >= 10) {
+                    iunlockput(ip);
+                    end_op();
+                    return -1;
+                }
+
+                char target[MAXPATH];
+                if (readi(ip, 0, (uint64)target, 0, MAXPATH) < 0) {
+                    iunlockput(ip);
+                    end_op();
+                    return -1;
+                }
+                
+                iunlockput(ip);
+
+                if ((ip = namei(target)) == 0) {
+                    end_op(); 
+                    return -1;
+                }
+                
+                ilock(ip);
+                depth++;
+            }
+        }
+
         if (ip->type == T_DIR && omode != O_RDONLY) {
             iunlockput(ip);
             end_op();
@@ -465,4 +494,27 @@ uint64 sys_pipe(void) {
         return -1;
     }
     return 0;
+}
+
+uint64 sys_symlink(void) {
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if (argstr(0, target, MAXPATH) < 0 ||
+      argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+
+  ip = create(path, T_SYMLINK, 0, 0);
+  if (ip == 0) {
+    end_op();
+    return -1;
+  }
+
+  writei(ip, 0, (uint64)target, 0, strlen(target));
+  iunlockput(ip);
+
+  end_op();
+  return 0;
 }
